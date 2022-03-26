7d30c07ef3427808ecf2517b430b6f139c41b347
napsterred <sid07ch@gmail.com>
2022-03-26 14:07:03 -0400

    Second

---
+++ src/app/chaudsid.spec.ts
@@ -0,0 +1,15 @@
+import { Chaudsid } from './chaudsid';
+
+describe('Chaudsid', () => {
+  it('should create an instance', () => {
+    expect(new Chaudsid()).toBeTruthy();
+  });
+});
+
+
+
+// const Chaudsid1 = (name:string) => {
+//   return name
+// }
+
+// console.log(Chaudsid1("dhfsdhofuilhsi"));
---
+++ src/app/content/content.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from 'src/app/courseInfo'
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+
+//@Course()
+
+
+
+export class ContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
